c9a909307c939c3fb049854007419c9994c244eb
Haris <mcmaster8888@gmail.com>
2021-04-30 13:26:54 +0100

    Added calendar into my front end design

---
+++ FinalProjectTimetable/FinalProjectTimetable/wwwroot/js/Timetable.js
@@ -0,0 +1,15 @@
+﻿function Calendar(timetable) {
+    var test1 = JSON.parse(timetable);
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,basicWeek,basicDay'
+        },
+        defaultDate: Date.now(),
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: test1
+    });
+}
